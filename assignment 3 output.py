2a5844252eb6119ee59729b2afbc21f21dd1e848
Prashant9216 <105444392+Prashant9216@users.noreply.github.com>
2022-05-12 22:27:36 +0530

    Add files via upload

---
+++ assignment 3 output.py
@@ -0,0 +1,51 @@
+Q1
+Enter a number:8
+Binary equivalent of 8 is 1000
+Q2
+Enter expression:3-9+55/5*6
+3-9+55/5*6 = 60.0
+Q3
+Enter an integer:3
+a) 35
+b) 3.0
+enter radius:9
+c) 1017.8760197630929
+Enter angle 1:25
+Enter angle 1:65
+d) 3.872358211124152
+Enter x coordinate of first point:4
+Enter y coordinate of first point:3
+Enter x coordinate of second point:1
+Enter y coordinate of second point:1
+e) 0.6666666666666666
+Q4
+4.(a)
+0
+1
+2
+3
+4
+4.(b)
+3
+4
+5
+6
+7
+8
+9
+4.(c)
+4
+7
+10
+4.(d)
+15
+13
+11
+9
+7
+4.(e)
+Q5
+Enter number of carbon atoms in molecule:2
+Enter number of hydrogen atoms in molecule:4
+Enter number of oxygen atoms in molecule:2
+The molecular weight of the compound is 60.051959999999994
