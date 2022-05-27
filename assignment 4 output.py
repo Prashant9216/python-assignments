dfa18702285835609ca368acde9be73055e6b7dc
Prashant9216 <105444392+Prashant9216@users.noreply.github.com>
2022-05-27 21:52:33 +0530

    Add files via upload

---
+++ assignment 4 output.py
@@ -0,0 +1,38 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: C:/Users/PRASHANT TIWARI/Desktop/assignment 4.py ==========
+Q1
+Enter mark:66
+Grade: B
+Continue?(y/n)n
+Q2
+Enter year:8542
+Not a leap year
+Q3
+Question 1:2x1=2
+Right!
+Question 2:5x3=15
+Right!
+Question 3:8x5=40
+Right!
+Question 4:9x5=45
+Right!
+Question 5:1x9=8
+Wrong. The correct answer is 9
+Question 6:2x8=99
+Wrong. The correct answer is 16
+Question 7:7x5=6
+Wrong. The correct answer is 35
+Question 8:8x1=8
+Right!
+Question 9:1x3=3
+Right!
+Question 10:6x1=6
+Right!
+Q4
+Answer: 177
+
+=========== RESTART: C:/Users/PRASHANT TIWARI/Desktop/assignment 4.py ==========
+Q1
+Enter mark:
